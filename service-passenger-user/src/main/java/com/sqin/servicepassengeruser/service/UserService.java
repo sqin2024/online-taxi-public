67e773029d7f39a60a3721b9f2465d1820c3c571
Sheng Qin <sqin@microfocus.com>
2025-04-27 13:39:03 +0800

    插入用戶

--- service-passenger-user/src/main/java/com/sqin/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/sqin/servicepassengeruser/service/UserService.java
@@ -6,6 +6,7 @@ import com.sqin.servicepassengeruser.mapper.PassengerUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,7 +24,18 @@ public class UserService {
         map.put("passenger_phone", passengerPhone);
         List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
 
-        System.out.println(passengerUsers.size() == 0 ? "無記錄" : passengerUsers.get(0).getPassengerPhone());
+        if (passengerUsers.size() == 0) {
+            PassengerUser passengerUser = new PassengerUser();
+            passengerUser.setPassengerName("三哥");
+            passengerUser.setPassengerGender((byte)0);
+            passengerUser.setPassengerPhone(passengerPhone);
+            passengerUser.setState((byte)0);
+            LocalDateTime now = LocalDateTime.now();
+            passengerUser.setGmtCreate(now);
+            passengerUser.setGmtModified(now);
+
+            passengerUserMapper.insert(passengerUser);
+        }
 
         return ResponseResult.success();
     }
