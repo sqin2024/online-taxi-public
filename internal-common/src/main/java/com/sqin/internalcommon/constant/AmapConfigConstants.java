4645387dacdf1808a26f9b7b97691de139e3e30a
Sheng Qin <sqin@microfocus.com>
2025-04-29 00:55:37 +0800

    拼接获取地区字典的Url

--- internal-common/src/main/java/com/sqin/internalcommon/constant/AmapConfigConstants.java
+++ internal-common/src/main/java/com/sqin/internalcommon/constant/AmapConfigConstants.java
@@ -4,6 +4,8 @@ public class AmapConfigConstants {
 
     public static final String DIRECTION_URL = "https://restapi.amap.com/v3/direction/driving";
 
+    public static final String DISTRICT_URL = "https://restapi.amap.com/v3/config/district";
+
     public static final String STATUS = "status";
     public static final String ROUTE = "route";
     public static final String PATHS = "paths";
