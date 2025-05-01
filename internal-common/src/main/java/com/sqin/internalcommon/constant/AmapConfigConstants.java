23f0ddce8e22f11126c5251e4e58b051ae0837cb
Sheng Qin <sqin@microfocus.com>
2025-05-01 15:45:30 +0800

    创建Service,获取sid，并作为常量放入yml文件

--- internal-common/src/main/java/com/sqin/internalcommon/constant/AmapConfigConstants.java
+++ internal-common/src/main/java/com/sqin/internalcommon/constant/AmapConfigConstants.java
@@ -6,6 +6,8 @@ public class AmapConfigConstants {
 
     public static final String DISTRICT_URL = "https://restapi.amap.com/v3/config/district";
 
+    public static final String SERVICE_ADD_URL = "https://tsapi.amap.com/v1/track/service/add";
+
     public static final String STATUS = "status";
 
     public static final String ROUTE = "route";
---
+++ mapInfo.md
@@ -0,0 +1,18 @@
+{
+  "errcode": 10000,
+  "errmsg": "OK",
+  "data": {
+    "name": "飞滴出行service",
+    "sid": 1048818
+  }
+}
+
+{
+  "errcode": 10000,
+  "errmsg": "OK",
+  "data": {
+    "name": "第一个终端",
+    "tid": 1274205213,
+    "sid": 1048818
+  }
+}
