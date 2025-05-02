ad9a154b99b0e9535dae3723c11d182f8917c400
Sheng Qin <sqin@microfocus.com>
2025-05-02 12:53:30 +0800

    创建新module,service-order

---
+++ api-passenger/src/main/java/com/sqin/apipassenger/controller/OrderController.java
@@ -0,0 +1,20 @@
+package com.sqin.apipassenger.controller;
+
+import com.sqin.internalcommon.dto.ResponseResult;
+import com.sqin.internalcommon.request.OrderRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @PostMapping("/add")
+    public ResponseResult add(@RequestBody OrderRequest orderRequest){
+        System.out.println(orderRequest);
+        return null;
+    }
+
+}
---
+++ internal-common/src/main/java/com/sqin/internalcommon/request/OrderRequest.java
@@ -0,0 +1,36 @@
+package com.sqin.internalcommon.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderRequest {
+
+    private String address;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime departTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime orderTime;
+
+    private String departure;
+
+    private String depLongitude;
+
+    private String depLatitude;
+
+    private String destination;
+
+    private String destLongitude;
+
+    private String destLatitude;
+
+    // 坐标加密标识， 1：gcj-02，2：wgs84，3：bd-09，4：cgcs2000北斗，0：其他
+    private Integer encrypt;
+
+    private String fareType;
+
+}
