10c80a82bde8fde279cf59863cf42512f8e266ce
Sheng Qin <sqin@microfocus.com>
2025-05-03 15:56:08 +0800

    添加一个前后端交互的模块

---
+++ sse-driver-client-web/src/main/java/com/sqin/ssedriverclientweb/SseDriverClientWebApplication.java
@@ -0,0 +1,13 @@
+package com.sqin.ssedriverclientweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseDriverClientWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseDriverClientWebApplication.class);
+    }
+
+}
